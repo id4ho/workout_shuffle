d48f4d6e58ce4346c0866dfc4da190ccffac251a
jackvnimble <jack.wilson.v@gmail.com>
2015-12-24 14:38:31 -0800

    Enforce uniqueness and min length on exercise names

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,8 +1,20 @@
 class Exercise < ActiveRecord::Base
-  validates :name, presence: true, uniqueness: true
+  validates_length_of :name, minimum: 3
+  validates_uniqueness_of :name
 
   has_many :muscle_targets
   has_many :muscle_groups, through: :muscle_targets
-  has_one :primary_target, -> { where(primary: true) }, class_name: "MuscleTarget", inverse_of: :exercise
+  has_one :primary_target, -> { where(primary: true) },
+                           class_name: "MuscleTarget",
+                           inverse_of: :exercise
   has_one :primary_muscle_group, through: :primary_target, source: :muscle_group
+
+  def name=(name)
+    formatted_name = name ? name.strip.downcase : name
+    super(formatted_name)
+  end
+
+  def display_name
+    name.titleize
+  end
 end
--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Exercise, type: :model do
-  context "associations" do
+  describe "associations" do
     it { should have_many(:muscle_targets) }
     it { should have_many(:muscle_groups) }
     it { should have_one(:primary_target).conditions(primary: true) }
@@ -9,6 +9,36 @@ RSpec.describe Exercise, type: :model do
       should have_one(:primary_muscle_group)
         .through(:primary_target)
         .source(:muscle_group)
+    end
+  end
+
+  describe "validations" do
+    it { should validate_length_of(:name).is_at_least(3) }
+
+    it "should enforce case-insensitive uniqueness" do
+      exercise = create(:exercise)
+      expect(exercise.name).to eq("squats")
+      exercise_dup = build(:exercise, name: "SQUATS")
+
+      expect(exercise_dup).to_not be_valid
+    end
+
+    it "should enforce uniqueness despite leading/trailing whitespace" do
+      exercise = create(:exercise)
+      expect(exercise.name).to eq("squats")
+      exercise_dup_one = build(:exercise, name: " squats")
+      exercise_dup_two = build(:exercise, name: "squats ")
+
+      expect(exercise_dup_one).to_not be_valid
+      expect(exercise_dup_two).to_not be_valid
+    end
+  end
+
+  describe "display_name" do
+    it "should titleize the name string for display purposes" do
+      exercise = create(:exercise, name: "awesome squats")
+
+      expect(exercise.display_name).to eq("Awesome Squats")
     end
   end
 end
