611e57a6adaa3cb6d807e99cc9dc26e16a00b2ff
jackvnimble <jack.wilson.v@gmail.com>
2015-12-28 15:35:23 -0800

    Generate a new random workout
    - The generated workout with have 1 cardio exercise and n
      resistance exercises, where n is the parameter passed

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -6,4 +6,19 @@ class Workout < ActiveRecord::Base
   validates_presence_of :user
   validates_presence_of :name
   validates_uniqueness_of :name, scope: [:user_id]
+
+  def self.generate_workout(num_resistance_exercises)
+    Workout.new.tap do |workout|
+      workout.add_random_resistance_exercises(num_resistance_exercises)
+      workout.add_random_cardio_exercise
+    end
+  end
+
+  def add_random_cardio_exercise
+    exercises << Exercises::CardioExercise.single_random
+  end
+
+  def add_random_resistance_exercises(num)
+    exercises << Exercises::ResistanceExercise.random_with_distinct_primary(num)
+  end
 end
--- spec/models/workout_spec.rb
+++ spec/models/workout_spec.rb
@@ -15,4 +15,28 @@ RSpec.describe Workout, type: :model do
       should validate_uniqueness_of(:name).scoped_to(:user_id)
     end
   end
+
+  describe ".generate_workout_for" do
+    before do
+      @exercise1 = create(:exercise)
+      @exercise2 = create(:exercise, name: "lunges")
+      @exercise3 = create(:exercise, name: "curls")
+      @cardio_exercise = create(:cardio_exercise)
+
+      expect(Exercises::ResistanceExercise).to receive(:random_with_distinct_primary)
+        .with(3) { [@exercise1, @exercise2, @exercise3] }
+    end
+
+    it "adds only one cardio exercise" do
+      new_workout = described_class.generate_workout(3)
+      expect(new_workout.exercises).to include(@cardio_exercise)
+    end
+
+    it "adds result from random_with_distinct_primary to exercises assoc." do
+      new_workout = described_class.generate_workout(3)
+      expect(new_workout.exercises).to include(
+        @exercise1, @exercise2, @exercise3
+      )
+    end
+  end
 end
