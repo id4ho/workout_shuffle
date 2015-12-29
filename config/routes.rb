af4544baea7f1314ebc04338a32f513004d17b44
jackvnimble <jack.wilson.v@gmail.com>
2015-12-28 18:08:39 -0800

    Root to a simple about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'home#about'
   resources :exercises
   resources :exercises_cardio_exercise, controller: :exercises
   resources :exercises_resistance_exercise, controller: :exercises
