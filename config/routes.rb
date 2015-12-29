0b7f6358cf27a5326663ebb16d28dd1dd5904e7a
jackvnimble <jack.wilson.v@gmail.com>
2015-12-28 17:04:03 -0800

    Adds destroy route for workouts

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :exercises_cardio_exercise, controller: :exercises
   resources :exercises_resistance_exercise, controller: :exercises
   resources :muscle_groups, only: :show
-  resources :workouts, only: [:index, :new, :show, :create]
+  resources :workouts, only: [:index, :new, :show, :create, :destroy]
 end
